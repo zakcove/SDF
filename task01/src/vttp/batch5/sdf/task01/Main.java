beec0be3130379e5d088f0ea32c16a465771d636
zakcove <muhdzakir@gmail.com>
2024-10-30 09:24:15 +0800

    SDF v1.0

---
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -0,0 +1,12 @@
+package vttp.batch5.sdf.task01;
+
+// Use this class as the entry point of your program
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.printf("hello, world\n");
+
+	}
+}
---
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -0,0 +1,9 @@
+package vttp.batch5.sdf.task02;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.printf("hello, world\n");
+	}
+}
